1550ae12341abbc3d6ff5a23b8f748c73c8ee7b6
unknown <eng.ahmedalabdli@gmail.com>
2024-06-12 00:40:23 +0300

    add update

---
+++ Backend-Dotnet/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library1.Models
+{
+    public partial class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public int? NumberOfPages { get; set; }
+        public DateTime? PublishedAt { get; set; }
+        public byte[]? Image { get; set; }
+        public byte[]? FilePdf { get; set; }
+        public string? Discription { get; set; }
+        public int? CategoryId { get; set; }
+    }
+}
